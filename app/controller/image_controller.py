e6fb5b28241f172d54852bc292b79c13eeb2b4be
Bakshish Arora <bakshish2231@gmail.com>
2025-01-18 19:57:17 +0530

    adds changes in controller and service

--- app/controller/image_controller.py
+++ app/controller/image_controller.py
@@ -1,11 +1,11 @@
 from flask import jsonify
 import bcrypt
 
-from app.database.db_connection import Database_operation
+from app.database.db_connection import DatabaseOperation
 from app.service.image_service import generate_return_dictionary
 from app.service.image_service import ImageService
 
-import bcrypt 
+import bcrypt
 import numpy as np
 import requests
 
@@ -20,28 +20,28 @@ class Controller:
     @classmethod
     def register_new_user(self, username, password, tokens):
         try:
-            hashed_pw = bcrypt.hashpw(password.encode('utf8'), bcrypt.gensalt())
+            hashed_pw = bcrypt.hashpw(
+                password.encode('utf8'), bcrypt.gensalt())
+
             # store the new user in database
-            Database_operation.insert_entry(username,hashed_pw,tokens)
-            
+            DatabaseOperation().insert_entry(username, hashed_pw, tokens)
             return True
-        
+
         except Exception as e:
             return jsonify(generate_return_dictionary(503, "Database Not Connected"))
-        
+
     @classmethod
     def url_based_image_classification(self, url):
 
         response = requests.get(url)
         img = Image.open(BytesIO(response.content))
-        # pre process the image 
+        # pre process the image
         img_array = ImageService.image_preprocess(img)
         # make predictions
-        actual_prediction = ImageClassification.classify(img_array)        
+        actual_prediction = ImageClassification.classify(img_array)
 
         return actual_prediction
 
     @classmethod
     def file_based_image_classification(self, file):
         pass
-
--- app/service/image_service.py
+++ app/service/image_service.py
@@ -1,26 +1,28 @@
-from app.database.db import users
 import bcrypt
 from app.config import Config
 from keras.applications.inception_v3 import preprocess_input
 from keras.preprocessing.image import img_to_array
 import numpy as np
+from app.database.db import db
 
 
 class Validations:
 
-    @classmethod
+    def __init__(self) -> None:
+        self.db = db
+        self.users = self.db.Users
+
     def user_exists(self, username):
-        if users.count_documents({"Username": username}) == 0:
-            return False 
+        if self.users.count_documents({"Username": username}) == 0:
+            return False
         else:
             return True
-        
-    @classmethod
-    def verify_pw(self,username, password):
+
+    def verify_pw(self, username, password):
         if not self.user_exists(username):
             return False
-        
-        hashed_pw = users.find({
+
+        hashed_pw = self.users.find({
             "Username": username
         })[0]["Password"]
 
@@ -28,38 +30,39 @@ class Validations:
             return True
         else:
             return False
-    
-    @classmethod
-    def verify_credentials(self,username, password):
+
+    def verify_credentials(self, username, password):
         if not self.user_exists(username):
             return generate_return_dictionary(301, "Invalid Username"), True
-        
+
         correct_pw = self.verify_pw(username, password)
 
         if not correct_pw:
             return generate_return_dictionary(302, "Incorrect Password"), True
-        
+
         return None, False
-    
-    @classmethod 
+
+    @classmethod
     def validate_admin_password(self, admin):
 
         if admin == Config.ADMIN_PASSWORD:
-            return True 
-        
+            return True
+
         return False
-    
+
+
 class ImageService:
 
     @classmethod
-    def image_preprocess(img):
+    def image_preprocess(cls, img):
 
-        img = img.resize((299,299))
+        img = img.resize((299, 299))
         img_array = img_to_array(img)
         img_array = np.expand_dims(img_array, axis=0)
         img_array = preprocess_input(img_array)
 
         return img_array
+
 
 def generate_return_dictionary(status, message):
     ret_json = {
@@ -67,5 +70,3 @@ def generate_return_dictionary(status, message):
         "message": message
     }
     return ret_json
-
-
